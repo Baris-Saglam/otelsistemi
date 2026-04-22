4bb9d8ea44d6db6efd256f5088c1a294904c1dbb
Baris-Saglam <barissaglam099@gmail.com>
2026-04-22 15:27:01 +0300

    Yaptığınız değişikliği anlatan açıklama mesajı

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const url = require('url');
 
 // Railway'in atadığı portu al, yoksa 3000'i kullan
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        // index.html dosyasını oku ve gönder
+    const parsedUrl = url.parse(req.url, true);
+    const pathname = parsedUrl.pathname;
+
+    // Ana sayfa
+    if (pathname === '/' || pathname === '/index.html') {
         fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
             if (err) {
                 res.writeHead(500);
@@ -17,7 +21,33 @@ const server = http.createServer((req, res) => {
                 res.end(content);
             }
         });
-    } else {
+    }
+    // Robots.txt
+    else if (pathname === '/robots.txt') {
+        fs.readFile(path.join(__dirname, 'robots.txt'), (err, content) => {
+            if (err) {
+                res.writeHead(404);
+                res.end('Dosya bulunamadı');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end(content);
+            }
+        });
+    }
+    // Sitemap.xml
+    else if (pathname === '/sitemap.xml') {
+        fs.readFile(path.join(__dirname, 'sitemap.xml'), (err, content) => {
+            if (err) {
+                res.writeHead(404);
+                res.end('Dosya bulunamadı');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'application/xml' });
+                res.end(content);
+            }
+        });
+    }
+    // Diğer dosyalar için 404
+    else {
         res.writeHead(404);
         res.end('Sayfa Bulunamadı');
     }
@@ -25,4 +55,6 @@ const server = http.createServer((req, res) => {
 
 server.listen(PORT, () => {
     console.log(`Sunucu ${PORT} portunda çalışıyor...`);
+    console.log(`Ana sayfa: http://localhost:${PORT}`);
+    console.log(`SEO dosyaları: http://localhost:${PORT}/robots.txt, http://localhost:${PORT}/sitemap.xml`);
 });
